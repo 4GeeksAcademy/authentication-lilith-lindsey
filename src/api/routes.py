8fa8a1004c92eeefb47c67cca22b8331f6607e13
Lilith Lindsey <lilithx1313@gmail.com>
2025-09-22 23:31:42 +0000

    Added a log in functionality

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,7 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from werkzeug.security import generate_password_hash, check_password_hash
 
 api = Blueprint('api', __name__)
 
@@ -27,7 +28,10 @@ def handle_sign_up():
     body = request.json
     new_email = body["email"]
     new_password = body["password"]
-    new_user = User(email = new_email, password = new_password)
+    new_user = User(
+        email = new_email, 
+        password = generate_password_hash(new_password)
+        )
 
     response_body = {
         "message": "New user successfully created!",
@@ -35,5 +39,27 @@ def handle_sign_up():
     }
     db.session.add(new_user)
     db.session.commit()
+
+    return jsonify(response_body), 200
+
+@api.route('/log_in', methods=['POST'])
+def handle_log_in():
+
+    body = request.json
+    new_email = body["email"]
+    new_password = body["password"]
+    user = User.query.filter_by(email = new_email).first()
+    if not user:
+        return jsonify("No matching email!"), 401
+    
+    if not check_password_hash(user.password, new_password):
+        return jsonify("Invalid password"), 401
+
+
+    response_body = {
+        "message": "Welcome, user.",
+        "user": user.serialize()
+    }
+   
 
     return jsonify(response_body), 200
